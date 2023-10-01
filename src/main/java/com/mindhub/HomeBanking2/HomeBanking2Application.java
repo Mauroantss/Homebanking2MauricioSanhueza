8a002912d10181f1d5418d47b8fe635d00d0e22d
Mauroantss <sanhuezamauricio.a@gmail.com>
2023-10-01 15:18:03 -0300

    Sprint1Java

---
+++ src/main/java/com/mindhub/HomeBanking2/HomeBanking2Application.java
@@ -0,0 +1,14 @@
+package com.mindhub.HomeBanking2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeBanking2Application {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(HomeBanking2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/mindhub/HomeBanking2/HomeBanking2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mindhub.HomeBanking2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeBanking2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
