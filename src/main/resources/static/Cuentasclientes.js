8362a6f6205f70f0c8f7648794ff8b88145bf63a
Mauroantss <sanhuezamauricio.a@gmail.com>
2023-10-04 23:53:04 -0300

    Sprint 2 (Muestra las cards pero no tengo contenido en ellas)

--- src/main/resources/static/Cuentasclientes.js
+++ src/main/resources/static/Cuentasclientes.js
@@ -1,53 +1,57 @@
-// Definir la URL de la API
-const apiUrl = 'http://localhost:8080/api/clients';
+const app = Vue.createApp({
+    data() {
+        return {
+            clients: [], // Almacenará los datos de los clientes
+            selectedClientId: null, // Almacenará el ID del cliente seleccionado
+            accounts: [] // Almacenará las cuentas del cliente seleccionado
+        };
+    },
+    methods: {
+        loadAccounts() {
+            // Verificamos si se ha seleccionado un cliente
+            if (!this.selectedClientId) {
+                return;
+            }
 
-// Función para crear una tarjeta de cuenta
-function createAccountCard(clientId, account) {
-    const card = document.createElement('div');
-    card.className = 'col-md-6 mb-4';
+            // Realizar una solicitud para obtener las cuentas del cliente seleccionado
+            const accountsUrl = `http://localhost:8080/accounts?clientId=${this.selectedClientId}`;
+            
+            fetch(accountsUrl)
+              .then(response => {
+                if (response.status === 200) {
+                  return response.json();
+                } else {
+                  throw new Error('No se pudo obtener los datos de las cuentas');
+                }
+              })
+              .then(accounts => {
+                this.accounts = accounts; // Actualizar las cuentas en Vue
+              })
+              .catch(error => {
+                console.error('Error al obtener las cuentas:', error);
+              });
+        }
+    },
+    mounted() {
+        // Realizar una solicitud para obtener los IDs de los clientes
+        const clientsUrl = 'http://localhost:8080/api/clients';
 
-    card.innerHTML = `
-        <div class="card">
-            <div class="card-body">
-                <h2 class="card-title">Cuenta de ${account.tipo}</h2>
-                <p class="card-text">Número de Cuenta: ${account.numero}</p>
-                <p class="card-text">Saldo Disponible: $${account.saldo}</p>
-            </div>
-        </div>
-    `;
-
-    return card;
-}
-
-// Realizar la solicitud GET utilizando fetch
-fetch(apiUrl)
-  .then(response => {
-    // Verificar si la respuesta es exitosa (código de estado 200)
-    if (response.status === 200) {
-      return response.json(); // Parsear la respuesta JSON
-    } else {
-      throw new Error('No se pudo obtener los datos de la API');
+        fetch(clientsUrl)
+          .then(response => {
+            if (response.status === 200) {
+              return response.json();
+            } else {
+              throw new Error('No se pudo obtener los IDs de los clientes');
+            }
+          })
+          .then(clients => {
+            this.clients = clients; // Actualizar los clientes en Vue
+          })
+          .catch(error => {
+            console.error('Error al obtener los IDs de los clientes:', error);
+          });
     }
-  })
-  .then(data => {
-    // Manejar los datos obtenidos de la API
-    console.log('Datos de clientes:', data);
+});
 
-    // Obtener el contenedor principal en el HTML
-    const mainContainer = document.getElementById('main-container');
-
-    // Iterar a través de los clientes y sus cuentas
-    data.forEach(client => {
-        client.account.forEach(account => {
-            // Crear una tarjeta de cuenta asociada al cliente
-            const card = createAccountCard(client.id, account);
-
-            // Agregar la tarjeta al contenedor principal
-            mainContainer.appendChild(card);
-        });
-    });
-  })
-  .catch(error => {
-    // Manejar errores de la solicitud
-    console.error('Error:', error);
-  });
+// Montamos la aplicación en el elemento con id "app"
+app.mount('#app');
