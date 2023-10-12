dd9aa4f92dd629884027b7c2b7ad4641b30fda99
Mauroantss <sanhuezamauricio.a@gmail.com>
2023-10-12 16:41:34 -0300

    Sprint 4 (Final)

--- src/main/resources/static/Js/Cuentasclientes.js
+++ src/main/resources/static/Js/Cuentasclientes.js
@@ -1,41 +1,58 @@
 const listClientsApp = Vue.createApp({
-  data() {
-      return {
-          clients: [],
-          selectedClientAccounts: [],
-          selectedClientName: ''
-      };
-  },
-  mounted() {
-      this.fetchClients();
-  },
-  methods: {
-      fetchClients() {
-          fetch('http://localhost:8080/api/clients')
-              .then(response => response.json())
+    data() {
+        return {
+            clients: [],
+            selectedClientAccounts: [],
+            selectedClientName: '',
+            selectedClientLoans: [],
+        };
+    },
+    mounted() {
+        this.fetchClients();
+    },
+    methods: {
+        fetchClients() {
+            fetch('http://localhost:8080/api/clients')
+                .then(response => response.json())
+                .then(data => {
+                    this.clients = data;
+                })
+                .catch(error => {
+                    console.error('Error al obtener clientes:', error);
+                });
+        },
+        
+        fetchClientLoans(clientId) {
+            fetch(`http://localhost:8080/rest/clients/${clientId}/clientLoans`)
+                .then(response => response.json())
+                .then(data => {
+                    this.selectedClientLoans = data["_embedded"]["clientLoans"];
+                })
+                .catch(error => {
+                    console.error('Error al obtener préstamos del cliente:', error);
+                });
+        },
+        
+        selectClient(clientId) {
+          fetch(`http://localhost:8080/api/clients/${clientId}`) 
+              .then(response => {
+                  if (!response.ok) {
+                      throw new Error('Network response was not ok');
+                  }
+                  return response.json();
+              })
               .then(data => {
-                  this.clients = data;
+                  this.selectedClientAccounts = data.accounts; 
+                  const client = this.clients.find(c => c.id === clientId);
+                  this.selectedClientName = `${client.firstName} ${client.lastName}`;
+                  
+                  // Llama al método para obtener los préstamos del cliente
+                  this.fetchClientLoans(clientId);
               })
               .catch(error => {
-                  console.error('Error al obtener clientes:', error);
+                  console.error('Error al obtener cuentas:', error);
               });
-      },
-      selectClient(clientId) {
-        fetch(`http://localhost:8080/api/clients/${clientId}`) // Asume que esta es la URL correcta
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                return response.json();
-            })
-            .then(data => {
-                this.selectedClientAccounts = data.accounts; // Asume que "accounts" es un campo directo en la respuesta
-                const client = this.clients.find(c => c.id === clientId);
-                this.selectedClientName = `${client.firstName} ${client.lastName}`;
-            })
-            .catch(error => {
-                console.error('Error al obtener cuentas:', error);
-              });
-      }
-  }
-}).mount('#create-client-app');
+        }
+    }
+  }).mount('#create-client-app');
+  
