a93a05e05cab57e465cca0f2638094e723616802
Mauroantss <sanhuezamauricio.a@gmail.com>
2023-10-27 11:29:36 -0300

    Sprint 7 (Final)

--- src/main/resources/static/web/js/create-card.js
+++ src/main/resources/static/web/js/create-card.js
@@ -3,28 +3,38 @@ const { createApp } = Vue;
 createApp({
   data() {
     return {
-      cardType: "",  // DEBIT o CREDIT
-      cardColor: "", // SILVER, GOLD, PLATINUM
-      error: "",     // Para almacenar cualquier mensaje de error
+      client: [],
+      cardType: [],
+      cardColor: [],
     };
   },
-  created() {
-    console.log("Aplicación Vue para Create Card creada");
-  },
+  created() {},
   methods: {
-    createCard() {
-      console.log("Intentando crear tarjeta con tipo", this.cardType, "y color", this.cardColor);
-      
-      axios.post('/api/clients/current/cards', `cardType=${this.typeCard}&cardColor=${this.colorCard}`)
+    getClient() {
+      axios
+        .get("/api/clients/current")
         .then((response) => {
-          console.log("Respuesta del servidor al crear la tarjeta:", response);
-          // Redirigir a cards.html después de crear la tarjeta
-          window.location = "/web/pages/cards.html";
+          this.client = response.data;
+          this.accounts = response.data.accounts;
+          setTimeout(() => (this.loading = false), 800);
         })
         .catch((error) => {
-          console.log("Error al intentar crear la tarjeta:", error);
-          this.error = "Hubo un error al intentar crear la tarjeta.";
+          error;
         });
-    }
-  }
+    },
+    createCard() {
+      axios
+        .post(
+          "/api/clients/current/cards",
+          `cardColor=${this.cardColor}&cardType=${this.cardType}`
+        )
+        .then((response) => {
+          location.pathname = "/web/pages/card.html";
+        })
+        .catch((error) => {
+          error;
+        });
+    },
+  },
+  computed: {},
 }).mount("#app");
